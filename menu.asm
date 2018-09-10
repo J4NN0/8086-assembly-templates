6141886ba852117c69082dbb7f79d3cfe68ac006
Federico Gianno <giannofederico@gmail.com>
2018-09-10 18:43:08 +0200

    Menù with choice

---
+++ menu.asm
@@ -0,0 +1,81 @@
+.model SMALL
+.stack
+.data
+
+STR1 db '1) Item 1 ', 10, 13, '$'
+STR2 db '2) Item 2 ', 10, 13, '$' 
+STR3 db '3) Item 3 ', 10, 13, '$'
+STR4 db '4) Item 4 ', 10, 13, '$'
+MSG_CHOICE db 'Enter your choice: ', '$'  
+MSG_ERR db 10, 13, 'This choice doesnt exist! ', 10, 13, '$'  
+       
+1C db 10, 13, '-> First part...', 10, 13, '$' 
+2C db 10, 13, '-> Second part...', 10, 13, '$'
+3C db 10, 13, '-> Third part...', 10, 13, '$'
+4C db 10, 13, '-> Forth part...', 10, 13, '$' 
+
+.code
+.startup
+
+MOV DX, OFFSET STR1
+MOV AH, 09
+INT 21H
+MOV DX, OFFSET STR2
+MOV AH, 09
+INT 21H
+MOV DX, OFFSET STR3
+MOV AH, 09
+INT 21H
+MOV DX, OFFSET MSG_CHOICE
+MOV AH, 09
+INT 21H
+
+MOV AH, 01
+INT 21H
+SUB AL, '0'
+
+CMP AL, 1
+JE FIRST
+CMP AL, 2
+JE SECOND
+CMP AL, 3
+JE THIRD
+CMP AL, 4
+JE FORTH
+
+FIRST: MOV DX, OFFSET 1C
+       MOV AH, 09
+       INT 21H
+       
+       ;CODE ...
+       
+       JMP END_MENU
+       
+SECOND: MOV DX, OFFSET 2C
+        MOV AH, 09
+        INT 21H
+       
+        ;CODE ...
+       
+        JMP END_MENU
+
+THIRD: MOV DX, OFFSET 3C
+       MOV AH, 09
+       INT 21H
+       
+       ;CODE ...
+       
+       JMP END_MENU
+       
+FORTH: MOV DX, OFFSET 4C
+       MOV AH, 09
+       INT 21H
+       
+       ;CODE ...
+       
+       JMP END_MENU 
+
+END_MENU:
+
+.exit
+end
